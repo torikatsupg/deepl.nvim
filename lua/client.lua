07b2b581682a22afdf0fd468bf767c778d7d97b5
torikatsupg <torikatsu923@gmail.com>
2023-03-28 07:52:32 +0900

    add log

--- lua/client.lua
+++ lua/client.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.hit_api(text)
   local url = "https://api-free.deepl.com/v2/translate"
   local cmd = string.format(
-    'curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "auth_key=%s&text=%s&target_lang=JA" %s | jq -r ".translations[0].text"',
+    'curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "auth_key=%s&text=%s&target_lang=JA" %s | tee > ~/Desktop/deepl.log | jq -r ".translations[0].text"',
     vim.env.deepl_nvim_apikey,
     api_key,
     text,

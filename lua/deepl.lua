4cd344e3bda53df5d835c498eab2e132653ce521
torikatsupg <torikatsu923@gmail.com>
2023-03-28 08:09:58 +0900

    change name

--- lua/deepl.lua
+++ lua/deepl.lua
@@ -2,13 +2,13 @@ local M = {}
 
 M.translate = function() 
   local strategy = require 'strategy'
-  local ui = require 'ui'
+  local ui = require 'deepl_ui'
   local mode = strategy.detect_mode()
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "v", false)
   vim.defer_fn(
     function()
       local lines          = strategy.get_text(mode)
-      local result         = require 'client'.hit_api(table.concat(lines, '\n'))
+      local result         = require 'deepl_client'.hit_api(table.concat(lines, '\n'))
       local row, col, _, _ = strategy.get_position()
       ui.show(lines, row, col, result)
     end, 0)
